f283f973710e7ec6b0f91934e7cc1162e2073642
Jean Mejicanos <jpmejicanos@ufm.edu>
2022-10-24 17:05:37 -0600

    first commit

---
+++ run.py
@@ -0,0 +1,36 @@
+from stem.control import Controller
+from flask import Flask, render_template
+
+
+#setting up the webpage service
+app = Flask("security_page")
+port = 5000
+host = "127.0.0.1"
+hidden_svc_dir = "c:/temp/"
+
+#here you can add routes
+@app.route('/')
+def index():
+    return render_template("home.html")
+
+@app.route('/home')
+def home():
+    return render_template("home.html")
+@app.route('/instructions')
+def instructions():
+    return render_template("instructions.html")
+    
+if __name__ == "__main__":
+    print(" * Getting controller")
+    controller = Controller.from_port(address="127.0.0.1", port=9151)
+    try:
+        controller.authenticate(password="")
+        controller.set_options([
+            ("HiddenServiceDir", hidden_svc_dir),
+            ("HiddenServicePort", "80 %s:%s" % (host, str(port)))
+            ])
+        svc_name = open(hidden_svc_dir + "/hostname", "r").read().strip()
+        print(" * Created host: %s" % svc_name)
+    except Exception as e:
+        print(e)
+    app.run()
